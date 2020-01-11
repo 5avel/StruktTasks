2626cceb2fb014829d13e0b594f0498bcb6108f2
5avel <5avel@hotmail.com>
2020-01-11 18:11:37 +0200

    TAsk4

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your birthday:");
+
+            string sDate = Console.ReadLine();
+
+            DateTime date;
+            date = DateTime.Parse(sDate);
+
+            int Years = DateTime.Now.Year - date.Year;
+            
+            date = date.AddYears(Years);
+            if (DateTime.Now > date)
+            {
+                date = date.AddYears(1);
+            }
+            TimeSpan dateTemp = date - DateTime.Now;
+
+            Console.WriteLine((int)dateTemp.TotalDays);
+        }
+    }
+}
