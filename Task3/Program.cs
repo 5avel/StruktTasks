b00986688034f601e4e2919e9b0038276ac1b2a0
5avel <5avel@hotmail.com>
2020-01-11 11:09:52 +0200

    task2

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,7 +6,25 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyClass myClass = new MyClass() { change = "не изменено" };
+            MyStruct myStruct = new MyStruct() { change = "не изменено" };
+            Console.WriteLine( $"myClass.change - {myClass.change}\nmyStruct.change - {myStruct.change}");
+
+            ClassTaker(myClass);
+            StruktTaker(myStruct);
+
+
+            Console.WriteLine($"myClass.change - {myClass.change}\nmyStruct.change - {myStruct.change}");
+        }
+
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
         }
     }
 }
